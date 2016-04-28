8de4793f69e5b241d9002c20758b62a8c632266b
zFxiang <zouxiangq@qq.com>
2016-04-28 11:30:31 +0800

    Initial version

--- app/src/main/java/cn/izouxiang/myview/MainActivity.java
+++ app/src/main/java/cn/izouxiang/myview/MainActivity.java
@@ -23,15 +23,12 @@ public class MainActivity extends AppCompatActivity {
         fv.setElements(elems);
     }
     public void onClick(View view){
-//        List<FunctionView.Element> elems = new ArrayList<>();
-//        elems.add(new FunctionView.Element(12f,6f));
-//        elems.add(new FunctionView.Element(10f,8f));
-//        elems.add(new FunctionView.Element(20f,6f));
-//        elems.add(new FunctionView.Element(11f,9f));
-//        fv.setmHeightCircleColor(Color.BLACK);
-//        fv.setmTextColor(Color.WHITE);
-//        fv.setmTextSize(20);
-//        fv.setElements(elems);
+        List<FunctionView.Element> elems = new ArrayList<>();
+        elems.add(new FunctionView.Element(12f,6f,"周一"));
+        elems.add(new FunctionView.Element(10f,8f,"周二"));
+        elems.add(new FunctionView.Element(20f,6f,"周三"));
+        elems.add(new FunctionView.Element(11f,9f,"周四"));
+        fv.setElements(elems);
         fv.startAnimation();
     }
 }
