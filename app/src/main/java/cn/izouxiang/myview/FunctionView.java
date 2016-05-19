d7ac28ad2b9136ed5f3423dba2f171a3ef998b83
zFxiang <zouxiangq@qq.com>
2016-05-19 21:43:04 +0800

    添加滑动以及改进绘制过程

--- app/src/main/java/cn/izouxiang/myview/FunctionView.java
+++ app/src/main/java/cn/izouxiang/myview/FunctionView.java
@@ -16,6 +16,7 @@ import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.List;
@@ -42,12 +43,17 @@ public class FunctionView extends View {
     private List<Element> elements;
     private boolean isDrawNum;
     private String numUnit;
+    private int maxCount;
     private float max;
     private float min;
     private int scaleX;
     private int scaleY;
     private int height;
     private int width;
+    private int defHeight;
+    private int defWidth;
+    private int mLastX;
+    private long lastTouchTime;
     private boolean drawFlag;
     private String TAG = "TEST";
     private int BOTTOM = 0;
@@ -55,6 +61,8 @@ public class FunctionView extends View {
     private float animationScale;
     private TypedArray ta;
     private ObjectAnimator mAnimator;
+    private int maxScrollX;
+    private int minScrollX = 0;
 
 
     public FunctionView(Context context) {
@@ -101,18 +109,132 @@ public class FunctionView extends View {
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        Log.d(TAG, "onSizeChanged: ...");
+        Log.d(TAG, "onSizeChanged: ..." + w + ":" + h);
+        defWidth = w;
+        defHeight = h;
+        if(elements != null && elements.size() > maxCount) {
+            w = (int) (w * 0.8 / maxCount * elements.size() + w * 0.2);
+        }
+        Log.d(TAG, "onSizeChanged: " + w);
         super.onSizeChanged(w, h, oldw, oldh);
-        height = getHeight();
-        width = getWidth();
+        height = h;
+        width = w;
+        maxScrollX = width - defWidth;
         drawFlag = true;
         measureScale();
     }
 
     @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int x = (int) event.getX();
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                Log.d(TAG, "onTouchEvent: ...");
+                mLastX = x;
+                //lastTouchTime = System.currentTimeMillis();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                //long end = System.currentTimeMillis();
+//                if( end - lastTouchTime < 10){
+//                    break;
+//                }
+                //lastTouchTime = end;
+                int dx = mLastX - x;
+                Log.d(TAG, "onTouchEvent: " + getScrollX());
+                if(getScrollX() < minScrollX  && dx < 0){
+                    dx = 0;
+                }
+                if(getScrollX() >maxScrollX && dx > 0){
+                    dx = 0;
+                }
+                if(dx < 0 && getScrollX() + dx < minScrollX){
+                    dx = minScrollX - getScrollX();
+                }
+                if(dx > 0 && getScrollX() + dx > maxScrollX){
+                    dx = maxScrollX - getScrollX();
+                }
+                scrollBy(dx,0);
+                mLastX = x;
+                break;
+        }
+        return true;
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
+        if(!drawFlag){
+            return ;
+        }
+        if (elements == null || elements.size() < 2) {
+            return;
+        }
+        float nowX,nowY1,nowY2;
+        Element nowElem;
+        Element nextElem = elements.get(0);
+        float nextX = (float) (0.1 * defWidth);
+        float nextY1 = (float) (0.2 * height + (max - nextElem.height) * scaleY) * animationScale;
+        float nextY2 = (float) (0.2 * height + (max - nextElem.low) * scaleY) * animationScale;
+        for (int i = 0; i < elements.size(); i++) {
+            nowX = nextX;
+            nowY1 = nextY1;
+            nowY2 = nextY2;
+            nowElem = nextElem;
+            //如果不是最后一个点
+            if(i < elements.size() - 1){
+                nextElem = elements.get(i + 1);
+                nextX = (float) (0.1 * defWidth + (i + 1) * scaleX);
+                nextY1 = (float) (0.2 * height + (max - nextElem.height) * scaleY) * animationScale;
+                nextY2 = (float) (0.2 * height + (max - nextElem.low) * scaleY) * animationScale;
+                //如果不是最后一个点，就画直线
+                drawLine(canvas,mHeightLinePaint,nowX,nowY1,nextX,nextY1);//上直线
+                drawLine(canvas,mLowLinePaint,nowX,nowY2,nextX,nextY2);//下直线
+            }
+            //画圆
+            canvas.drawCircle(nowX,nowY1,mRadius,mHeightCirclePaint);//上圆
+            canvas.drawCircle(nowX,nowY2,mRadius,mLowCirclePaint);//下圆
+            //画值
+            if(isDrawNum) {
+                String value = String.valueOf(nowElem.height) + numUnit;
+                canvas.drawText(value , nowX - mRadius, nowY1 -mRadius *2 , mTextPaint);//上值
+                value = String.valueOf(nowElem.low) + numUnit;
+                Rect bounds = new Rect();
+                mTextPaint.getTextBounds(value, 0, value.length(), bounds);
+                canvas.drawText(
+                        value ,
+                        nowX - mRadius, nowY2 + bounds.height() + mRadius * 2 ,
+                        mTextPaint);//下值
+            }
+            //画标题
+            String title = nowElem.title;
+            if(!TextUtils.isEmpty(title)){
+                Rect bounds = new Rect();
+                mTextPaint.getTextBounds(title,0,title.length(),bounds);
+                canvas.drawText(
+                        nowElem.title,
+                        nowX - bounds.width() / 2,
+                        (float) (0.9 * height),
+                        mTextPaint
+                );
+            }
+        }
+    }
+
+    private void drawLine(Canvas canvas,Paint paint,float x1, float y1, float x2, float y2){
+        if(x1 != x2 || y1 != y2) {
+            float scale = (float) (mRadius / Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
+            Path path = new Path();
+            path.moveTo(x1 + (x2 - x1) * scale, y1 + (y2 - y1) * scale);
+            path.lineTo(x2 + (x1 - x2) * scale, y2 + (y1 - y2) * scale);
+            canvas.drawPath(path, paint);
+        }
+    }
+
+
+    protected void onDraw1(Canvas canvas) {
+        if(!drawFlag){
+            return ;
+        }
         Log.d(TAG, "onDraw: ...");
-        super.onDraw(canvas);
         if (elements == null || elements.size() < 2) {
             return;
         }
@@ -120,7 +242,7 @@ public class FunctionView extends View {
             Element elem1 = elements.get(i);
             // 如果是最后一个点，点1点2为同一个点；
             if (elements.size() - 1 == i) {
-                float x = (float) (0.1 * width + i * scaleX);
+                float x = (float) (0.1 * defWidth + i * scaleX);
                 float y = (float) (0.2 * height + (max - elem1.height) * scaleY) * animationScale;
                 drawFunction(
                         canvas,
@@ -148,28 +270,29 @@ public class FunctionView extends View {
                         canvas,
                         TOP,
                         elem1.height,
-                        (float) (0.1 * width + i * scaleX),
+                        (float) (0.1 * defWidth + i * scaleX),
                         (float) (0.2 * height + (max - elem1.height) * scaleY) * animationScale,
-                        (float) (0.1 * width + (i + 1) * scaleX),
+                        (float) (0.1 * defWidth + (i + 1) * scaleX),
                         (float) (0.2 * height + (max - elem2.height) * scaleY) * animationScale
                 );
                 drawFunction(
                         canvas,
                         BOTTOM,
                         elem1.low,
-                        (float) (0.1 * width + i * scaleX),
+                        (float) (0.1 * defWidth + i * scaleX),
                         (float) (0.2 * height + (max - elem1.low) * scaleY) * animationScale,
-                        (float) (0.1 * width + (i + 1) * scaleX),
+                        (float) (0.1 * defWidth + (i + 1) * scaleX),
                         (float) (0.2 * height + (max - elem2.low) * scaleY) * animationScale
                 );
             }
+            //绘制标题
             String title = elem1.title;
             if(!TextUtils.isEmpty(title)){
                 Rect bounds = new Rect();
                 mTextPaint.getTextBounds(title,0,title.length(),bounds);
                 canvas.drawText(
                         elem1.title,
-                        (float) (0.1 * width + i * scaleX) - bounds.width() / 2,
+                        (float) (0.1 * defWidth + i * scaleX) - bounds.width() / 2,
                         (float) (0.9 * height),
                         mTextPaint
                         );
@@ -178,40 +301,10 @@ public class FunctionView extends View {
 
     }
 
-    /**
-     * 测量比例
-     */
-
-    private void measureScale() {
-        if (elements == null || elements.size() < 2) {
-            return;
-        }
-        if (!drawFlag) {
-            return;
-        }
-        Log.d(TAG, "measureScale: ...");
-        max = elements.get(0).height;
-        min = elements.get(0).low;
-        for (Element element : elements) {
-            if (element.height > max) {
-                max = element.height;
-            }
-            if (element.low < min) {
-                min = element.low;
-            }
-        }
-        if (max == min) {
-            throw new RuntimeException("elements max value can't equal min value ");
-        }
-        scaleX = (int) ((width * 0.8) / (elements.size() - 1));
-        scaleY = (int) ((height * 0.5) / (max - min));
-
-    }
-
     //画两个点之间的连接线，并且以第一个点为圆心画圆
     private void drawFunction(Canvas canvas, int drawType, float value, float x1, float y1, float x2, float y2) {
-        Paint circlePaint = null;
-        Paint linePaint = null;
+        Paint circlePaint ;
+        Paint linePaint;
         String text = String.valueOf(value) + numUnit;
         float textX,textY;
         if (drawType == TOP) {
@@ -244,12 +337,44 @@ public class FunctionView extends View {
         }
     }
 
+
+    /**
+     * 测量比例
+     */
+
+    private void measureScale() {
+        if (elements == null || elements.size() < 2) {
+            return;
+        }
+        if (!drawFlag) {
+            return;
+        }
+        Log.d(TAG, "measureScale: ...");
+        max = elements.get(0).height;
+        min = elements.get(0).low;
+        for (Element element : elements) {
+            if (element.height > max) {
+                max = element.height;
+            }
+            if (element.low < min) {
+                min = element.low;
+            }
+        }
+        if (max == min) {
+            throw new RuntimeException("elements max value can't equal min value ");
+        }
+        scaleX = (int) ((width - defWidth * 0.2) / (elements.size() - 1));
+        scaleY = (int) ((height * 0.5) / (max - min));
+
+    }
+
+
     /**
      * 初始化View
      */
     public void initView() {
         Log.d(TAG, "initView: ...");
-        animationScale = 1l;
+        animationScale = 1f;
         drawFlag = false;
         if(null == ta) {
             isDrawNum = true;
@@ -262,15 +387,20 @@ public class FunctionView extends View {
             mLowCircleColor = Color.GREEN;
             mTextSize = 16f;
             mTextColor = Color.YELLOW;
+            maxCount = 5;
         }else{
+            maxCount = ta.getInt(R.styleable.FunctionView_maxCount,5);
             numUnit = ta.getString(R.styleable.FunctionView_numUnit);
+            if(null == numUnit){
+                numUnit = "";
+            }
             isDrawNum = ta.getBoolean(R.styleable.FunctionView_isDrawNum,true);
             mRadius = ta.getFloat(R.styleable.FunctionView_radius,10f);
             mLineStrokeWidth = ta.getFloat(R.styleable.FunctionView_lineStrokeWidth,8f);
             mCircleStrokeWidth = ta.getFloat(R.styleable.FunctionView_circleStrokeWidth,8f);
             mHeightLineColor = ta.getColor(R.styleable.FunctionView_heightLineColor,Color.RED);
             mHeightCircleColor = ta.getColor(R.styleable.FunctionView_heightCircleColor,Color.GREEN);
-            mLowLineColor = ta.getColor(R.styleable.FunctionView_lowLineColor,Color.RED);;
+            mLowLineColor = ta.getColor(R.styleable.FunctionView_lowLineColor,Color.RED);
             mLowCircleColor = ta.getColor(R.styleable.FunctionView_lowCircleColor,Color.GREEN);
             mTextSize = ta.getFloat(R.styleable.FunctionView_textSize,16f);
             mTextColor = ta.getColor(R.styleable.FunctionView_textColor,Color.YELLOW);
@@ -385,6 +515,16 @@ public class FunctionView extends View {
         return numUnit;
     }
 
+    public int getMaxCount() {
+        return maxCount;
+    }
+
+    public void setMaxCount(int maxCount) {
+        this.maxCount = maxCount;
+        onSizeChanged(defWidth,defHeight,width,height);
+        scrollTo(0,0);
+    }
+
     public void setNumUnit(String numUnit) {
         this.numUnit = numUnit;
         invalidate();
@@ -450,14 +590,19 @@ public class FunctionView extends View {
         invalidate();
     }
 
-    public void setElements(List<Element> elements) {
+    public void setElements(@NonNull List<Element> elements) {
         Log.d(TAG, "setElements: ...");
         if (elements == null || elements.size() < 2) {
             throw new RuntimeException("elements size must >= 2");
         }
-        this.elements = elements;
-        measureScale();
-        invalidate();
+        if(this.elements == null || this.elements.size() == elements.size()) {
+            this.elements = elements;
+            measureScale();
+        }else {
+            this.elements = elements;
+            onSizeChanged(defWidth,defHeight,width,height);
+        }
+        scrollTo(0,0);
     }
 
     public void setAnimationScale(float animationScale) {
--- app/src/main/java/cn/izouxiang/myview/MainActivity.java
+++ app/src/main/java/cn/izouxiang/myview/MainActivity.java
@@ -4,28 +4,34 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private FunctionView fv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        fv = (FunctionView) findViewById(R.id.fv);
-//        List<FunctionView.Element> elems = new ArrayList<>();
-//        elems.add(new FunctionView.Element(10f,8f,"周一"));
-//        elems.add(new FunctionView.Element(12f,6f,"周二"));
-//        elems.add(new FunctionView.Element(11f,9f,"周三"));
-//        elems.add(new FunctionView.Element(14f,9f,"周四"));
-//        elems.add(new FunctionView.Element(20f,6f,"周五"));
-//        fv.setElements(elems);
+        fv = (FunctionView) findViewById(R.id.fv);
+        List<FunctionView.Element> elems = new ArrayList<>();
+        elems.add(new FunctionView.Element(10f,8f,"周一"));
+        elems.add(new FunctionView.Element(12f,6f,"周二"));
+        elems.add(new FunctionView.Element(11f,9f,"周三"));
+        elems.add(new FunctionView.Element(14f,9f,"周四"));
+        elems.add(new FunctionView.Element(20f,6f,"周五"));
+        elems.add(new FunctionView.Element(11f,9f,"周三"));
+        elems.add(new FunctionView.Element(14f,9f,"周四"));
+        elems.add(new FunctionView.Element(20f,6f,"周五"));
+        fv.setElements(elems);
     }
     public void onClick(View view){
-//        List<FunctionView.Element> elems = new ArrayList<>();
-//        elems.add(new FunctionView.Element(12f,6f,"周一"));
-//        elems.add(new FunctionView.Element(10f,8f,"周二"));
-//        elems.add(new FunctionView.Element(20f,6f,"周三"));
-//        elems.add(new FunctionView.Element(11f,9f,"周四"));
-//        fv.setElements(elems);
-//        fv.startAnimation();
+        List<FunctionView.Element> elems = new ArrayList<>();
+        elems.add(new FunctionView.Element(12f,6f,"周一"));
+        elems.add(new FunctionView.Element(10f,8f,"周二"));
+        elems.add(new FunctionView.Element(20f,6f,"周三"));
+        elems.add(new FunctionView.Element(11f,9f,"周四"));
+        fv.setElements(elems);
+        fv.startAnimation();
     }
 }
